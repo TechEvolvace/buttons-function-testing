c865f5df55b676f6d5b9abb6a10ada6757cb054c
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-10-13 23:43:36 -0500

    Fixed issue with not loading color switch for Experiment 8!

--- experiment-8-toggle-mode-switch.js
+++ experiment-8-toggle-mode-switch.js
@@ -14,23 +14,23 @@
 */
 
 //  Getting a reference of Experiment 8's mode switch toggle button by its id value
-const colorSchemeSwitch = document.getElementById("toggle-switch");
+const colorSchemeToggleSwitch = document.getElementById("toggle-switch");
 
 /* 
     Getting a reference of the section in Experiment 8 with all the components
     that the light dark mode will affect by its id value 
 */
-const affectedComponents = document.getElementById('experiment-8-interactable-section');
+const modifiedComponents = document.getElementById('experiment-8-interactable-section');
 
 /*  Getting a reference of the message about the current mode in Experiment 8! */
-const currentModeMessage = document.getElementById('experiment-8-message');
+const currentModeMessageArea = document.getElementById('experiment-8-message');
 
 /*  Getting a reference of the message about the current mode in Experiment 8! */
-let savedMode = localStorage.getItem('theme');
+let currentSavedMode = localStorage.getItem('theme');
 
 // Then, will either applies the saved mode or defaults to light mode
-if(savedMode){
-    setMode(savedMode);
+if(currentSavedMode){
+    setMode(currentSavedMode);
 } else {
     setMode('light');
 }
@@ -41,11 +41,11 @@ if(savedMode){
 */
 function setMode(mode){
     if(mode === 'dark'){
-        affectedComponents.classList.add('darkMode');
-        affectedComponents.classList.remove('lightMode');
+        modifiedComponents.classList.add('darkMode');
+        modifiedComponents.classList.remove('lightMode');
     } else {
-        affectedComponents.classList.add('lightMode');
-        affectedComponents.classList.remove('darkMode');
+        modifiedComponents.classList.add('lightMode');
+        modifiedComponents.classList.remove('darkMode');
     }
 }
 
@@ -53,21 +53,26 @@ function setMode(mode){
     Toggle between light and dark mode with a mouse click on the toggle button 
     and apply the changes on Experiment 7's components 
 */
-colorSchemeSwitch.addEventListener('click', () => {
+colorSchemeToggleSwitch.addEventListener('click', () => {
     /*
         Checks if darkMode class is currently used. If so, it's currently dark mode; 
         otherwise, it's currently light mode! 
     */
-    let currentMode = affectedComponents.classList.contains('darkMode') ? 'dark' : 'light';
+    let currentMode = modifiedComponents.classList.contains('darkMode') ? 'dark' : 'light';
 
     //  Switch to the other mode 
     if(currentMode === 'dark'){
         setMode('light');
         localStorage.setItem('theme', 'light');
-        currentModeMessage.textContent = `Welcome to Light Mode!`;
+        currentModeMessageArea.textContent = `Welcome to Light Mode!`;
     } else {
         setMode('dark');
         localStorage.setItem('theme', 'dark');
-        currentModeMessage.textContent = `Welcome to Dark Mode!`;
+        currentModeMessageArea.textContent = `Welcome to Dark Mode!`;
     }
+});
+
+const showCurrentMode = document.getElementById("show-current-mode");
+showCurrentMode.addEventListener("click", () => {
+   console.log(`Current mode: ${savedMode}`);
 });
